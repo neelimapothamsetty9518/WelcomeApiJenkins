dad3109a532a7ebbc0627fd27ec6fd1d0b2478ca
neelimapothamsetty9518 <neelimapothamsetty1@gmail.com>
2024-02-15 12:51:20 +0530

    added files

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp1;
+
+Console.WriteLine("Welcome to auth app");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@1256"));
---
+++ SignIn.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp1
+{
+    public class SignIn
+    {
+        public  string Authenticate(string Username,string Userpwd)
+        {
+            string Msg;
+            if (String.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Userpwd))
+            {
+                Msg = "username and pasword required";
+            }
+            else
+            {
+                if ((Username == "admin") && (Userpwd == "admin@1256"))
+                {
+                    Msg = "Auth pass";
+                }
+                else
+                {
+                    Msg = "fail";
+
+                }
+            }
+                return Msg;
+            
+        }
+    }
+}
